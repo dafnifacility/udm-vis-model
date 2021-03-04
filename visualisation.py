d8054f79398e96ce2db00a82b4a54616cecdf709
Rose Dickinson <rose.dickinson@stfc.ac.uk>
2021-03-04 15:11:02 +0000

    fix: main args and run black

--- visualisation.py
+++ visualisation.py
@@ -23,26 +23,25 @@ matplotlib.rcParams["font.size"] = 16
 # Constants for plotting
 #
 EXTENTS = {
-    'arc': (418_000, 573_000, 170_000, 325_000),
-    'cty': (475_000, 500_000, 225_000, 250_000),
-    'ngb': (487_000, 492_000, 231_500, 236_500)
+    "arc": (418_000, 573_000, 170_000, 325_000),
+    "cty": (475_000, 500_000, 225_000, 250_000),
+    "ngb": (487_000, 492_000, 231_500, 236_500),
 }
-GREEN = '#219653'
-BLUE = '#2D9CDB'
-YELLOW = '#F2C94C'
-RED = '#EB5757'
-GREY = '#DDDDDD'
+GREEN = "#219653"
+BLUE = "#2D9CDB"
+YELLOW = "#F2C94C"
+RED = "#EB5757"
+GREY = "#DDDDDD"
 
 
-def main(base_folder):
-    """Plot images from OpenUDM outputs
-    """
+def main(base_folder, output_folder):
+    """Plot images from OpenUDM outputs"""
     print("Data contents", os.listdir(base_folder))
-    suitability_path = os.path.join(base_folder, 'out_cell_suit.asc')
-    dwellings_path = os.path.join(base_folder, 'out_cell_dph.asc')
-    development_path = os.path.join(base_folder, 'out_cell_dev.asc')
+    suitability_path = os.path.join(base_folder, "out_cell_suit.asc")
+    dwellings_path = os.path.join(base_folder, "out_cell_dph.asc")
+    development_path = os.path.join(base_folder, "out_cell_dev.asc")
 
-    outline_path = os.path.join(base_folder, 'arc-outline.gpkg')
+    outline_path = os.path.join(base_folder, "arc-outline.gpkg")
     with fiona.open(outline_path, "r") as shapefile:
         arc_mask = [feature["geometry"] for feature in shapefile]
 
@@ -61,8 +60,15 @@ def main(base_folder):
 
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", YELLOW])
 
-    for zoom in ('arc', 'cty', 'ngb'):
-        plot_map(data, data_extent, EXTENTS[zoom], cmap=cmap, norm=norm, label="Combined Suitability Score")
+    for zoom in ("arc", "cty", "ngb"):
+        plot_map(
+            data,
+            data_extent,
+            EXTENTS[zoom],
+            cmap=cmap,
+            norm=norm,
+            label="Combined Suitability Score",
+        )
         output_fname = os.path.join(output_folder, f"suitability_{zoom}.png")
         plt.savefig(output_fname)
         plt.close()
@@ -72,7 +78,9 @@ def main(base_folder):
     #
     vmin, vmax = file_raster_min_max(dwellings_path)
     norm = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
-    plot_development_and_dwellings(base_folder, dwellings_path, development_path, norm, arc_mask)
+    plot_development_and_dwellings(
+        base_folder, dwellings_path, development_path, norm, arc_mask
+    )
 
 
 def files_raster_min_max(paths, mask=None):
@@ -100,28 +108,40 @@ def file_raster_min_max(fname, mask=None, vmin=0, vmax=0):
     return vmin, vmax
 
 
-def plot_map(raster, raster_extent, extent, label=None, cmap='Greens', norm=None):
+def plot_map(raster, raster_extent, extent, label=None, cmap="Greens", norm=None):
     osgb = ccrs.epsg(27700)
-    fig, ax = plt.subplots(subplot_kw={'projection':osgb}, figsize=(12, 13))
+    fig, ax = plt.subplots(subplot_kw={"projection": osgb}, figsize=(12, 13))
     ax.set_frame_on(False)
     ax = plt.axes([0, 0.07, 1, 1], projection=osgb)
     ax.set_extent(extent, crs=osgb)
-    ax.set_frame_on(False) # don't draw axes outline/background rectangle
+    ax.set_frame_on(False)  # don't draw axes outline/background rectangle
 
     # add the image
-    ax.imshow(raster, origin='upper', extent=raster_extent, transform=osgb, cmap=cmap, norm=norm)
+    ax.imshow(
+        raster,
+        origin="upper",
+        extent=raster_extent,
+        transform=osgb,
+        cmap=cmap,
+        norm=norm,
+    )
 
     # add the colorbar
     cax = inset_axes(
         ax,
         width="40%",
         height="3%",
-        loc='lower left',
+        loc="lower left",
         bbox_to_anchor=(0.05, -0.05, 1, 1),
         bbox_transform=ax.transAxes,
         borderpad=0,
     )
-    fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), cax=cax, label=label, orientation='horizontal')
+    fig.colorbar(
+        matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap),
+        cax=cax,
+        label=label,
+        orientation="horizontal",
+    )
     return ax
 
 
@@ -149,37 +169,75 @@ def plot_development_and_dwellings(base_folder, fname, dev_fname, norm, arc_mask
     reds.set_under(color=(1, 1, 1, 0))
     blues.set_under(color=(1, 1, 1, 0))
 
-    for zoom in ('arc', 'cty', 'ngb'):
+    for zoom in ("arc", "cty", "ngb"):
         osgb = ccrs.epsg(27700)
-        fig, ax = plt.subplots(subplot_kw={'projection':osgb}, figsize=(12, 13))
+        fig, ax = plt.subplots(subplot_kw={"projection": osgb}, figsize=(12, 13))
         ax.set_frame_on(False)
         ax = plt.axes([0, 0.07, 1, 1], projection=osgb)
         ax.set_frame_on(False)
         ax.set_extent(EXTENTS[zoom], crs=osgb)
 
-        ax.imshow(dev_data, origin='upper', extent=data_extent, transform=osgb, cmap=blues, norm=dev_norm)
-        ax.imshow(data, origin='upper', extent=data_extent, transform=osgb, cmap=reds, norm=norm)
+        ax.imshow(
+            dev_data,
+            origin="upper",
+            extent=data_extent,
+            transform=osgb,
+            cmap=blues,
+            norm=dev_norm,
+        )
+        ax.imshow(
+            data,
+            origin="upper",
+            extent=data_extent,
+            transform=osgb,
+            cmap=reds,
+            norm=norm,
+        )
 
         # add the colorbar
         cax = inset_axes(
             ax,
             width="40%",
             height="3%",
-            loc='lower left',
+            loc="lower left",
             bbox_to_anchor=(0.05, -0.05, 1, 1),
             bbox_transform=ax.transAxes,
             borderpad=0,
         )
-        fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=reds), cax=cax,
-                     orientation='horizontal',
-                     label="New Dwellings (per hectare)")
+        fig.colorbar(
+            matplotlib.cm.ScalarMappable(norm=norm, cmap=reds),
+            cax=cax,
+            orientation="horizontal",
+            label="New Dwellings (per hectare)",
+        )
 
         # add extra legend bits
         legend_elements = [
-            Line2D([0], [0], marker='o', color='w', label='No dwellings', markerfacecolor='#dff0f9', markersize=10),
-            Line2D([0], [0], marker='o', color='w', label='Current development', markerfacecolor='#95cdec', markersize=10),
+            Line2D(
+                [0],
+                [0],
+                marker="o",
+                color="w",
+                label="No dwellings",
+                markerfacecolor="#dff0f9",
+                markersize=10,
+            ),
+            Line2D(
+                [0],
+                [0],
+                marker="o",
+                color="w",
+                label="Current development",
+                markerfacecolor="#95cdec",
+                markersize=10,
+            ),
         ]
-        ax.legend(handles=legend_elements, bbox_to_anchor=(0.55, -0.075), loc='lower left', borderaxespad=0.)
+        ax.legend(
+            handles=legend_elements,
+            bbox_to_anchor=(0.55, -0.075),
+            loc="lower left",
+            borderaxespad=0.0,
+        )
 
         output_fname = os.path.join(base_folder, f"dwellings_{zoom}.png")
         plt.savefig(output_fname)
@@ -189,9 +247,9 @@ def plot_development_and_dwellings(base_folder, fname, dev_fname, norm, arc_mask
         plt.close()
 
 
-if __name__ == '__main__':
-    with open("config.json", 'r') as fh:
-        config  = json.load(fh)
-        base_folder = config['data_folder']
-        output_folder = config['output_folder']
+if __name__ == "__main__":
+    with open("config.json", "r") as fh:
+        config = json.load(fh)
+        base_folder = config["data_folder"]
+        output_folder = config["output_folder"]
     main(base_folder, output_folder)
